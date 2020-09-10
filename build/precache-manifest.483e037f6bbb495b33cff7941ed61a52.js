985192402e31a5357fffe8a8e49219eb977b7b65
avinashow <avinashow@users.noreply.github.com>
2020-09-10 00:06:06 -0700

    build files

---
+++ build/precache-manifest.483e037f6bbb495b33cff7941ed61a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbc9515cc536eace6e102eca2192c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5a46446a35b02e7fd6",
+    "url": "/static/css/main.cc40955d.chunk.css"
+  },
+  {
+    "revision": "ade6ba81e4d386312e90",
+    "url": "/static/js/2.edbf6227.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.edbf6227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5a46446a35b02e7fd6",
+    "url": "/static/js/main.e5ccca10.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
